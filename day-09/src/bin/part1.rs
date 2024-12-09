aba939b7f2f0e640ff7307ba7117122bfa219923
Gregory Cavelier <gregory.cavelier@cnieg.fr>
2024-12-09 17:02:03 +0100

    day 09 - part 1 - using a struct

--- day-09/src/bin/part1.rs
+++ day-09/src/bin/part1.rs
@@ -1,6 +1,11 @@
 use itertools::Itertools;
 use tracing::{debug, error, info, instrument};
 
+#[derive(Debug, Clone)]
+struct FileBlock {
+    file_id: Option<u32>,
+}
+
 #[instrument(skip_all)]
 fn main() {
     tracing_subscriber::fmt::init();
@@ -11,13 +16,13 @@ fn main() {
 }
 
 #[instrument(skip_all)]
-fn reord(blocks: &mut Vec<char>) {
+fn reord(blocks: &mut Vec<FileBlock>) {
     for idx in 0..blocks.len() {
-        if blocks[idx] == '.' {
+        if blocks[idx].file_id.is_none() {
             let idx_to_swap = blocks
                 .iter()
                 .enumerate()
-                .filter(|(_, c)| **c != '.')
+                .filter(|(_, b)| b.file_id.is_some())
                 .map(|(i, _)| i)
                 .rev()
                 .next()
@@ -33,22 +38,23 @@ fn reord(blocks: &mut Vec<char>) {
 fn part1(input: &str) -> usize {
     let mut file_len = true;
     let mut idx: u32 = 0;
-    let mut blocks: Vec<char> = input
+    let mut blocks: Vec<FileBlock> = input
         .chars()
         .filter(|c| *c != '\n')
         .flat_map(|c| {
             let capa = c.to_digit(10).unwrap() as usize;
             let mut blocks = Vec::with_capacity(capa);
+            let file_block = FileBlock { file_id: Some(idx) };
             if file_len {
                 // file space length
                 for _ in 0..capa {
-                    blocks.push(char::from_u32(idx + 0x30).unwrap());
+                    blocks.push(file_block.clone());
                 }
                 idx += 1;
             } else {
                 // free space length
                 for _ in 0..capa {
-                    blocks.push('.');
+                    blocks.push(FileBlock { file_id: None });
                 }
             }
             file_len = !file_len;
@@ -64,9 +70,9 @@ fn part1(input: &str) -> usize {
     blocks
         .iter()
         .enumerate()
-        .filter(|(_, c)| **c != '.')
+        .filter(|(_, b)| b.file_id.is_some())
         //.inspect(|(idx, c)| info!("{c} {}", **c as isize))
-        .map(|(idx, c)| idx * (*c as usize - 0x30))
+        .map(|(idx, b)| idx * (b.file_id.unwrap() as usize))
         .sum()
 }
 
