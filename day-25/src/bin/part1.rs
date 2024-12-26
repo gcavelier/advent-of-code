193a422a82c99480303982bf376cf80c2a7e4b78
Gregory Cavelier <gregory.cavelier@cnieg.fr>
2024-12-26 12:02:08 +0100

    day 25 - part 1

--- day-25/src/bin/part1.rs
+++ day-25/src/bin/part1.rs
@@ -14,18 +14,46 @@ fn main() {
 
 #[instrument(skip_all)]
 fn part1(input: &str) -> usize {
-    let mut is_lock = false;
+    let mut locks: Vec<[i32; WIDTH]> = Vec::new();
+    let mut keys: Vec<[i32; WIDTH]> = Vec::new();
+
     for x in input.split("\n\n") {
         let mut tmp_arr = [0; WIDTH];
+        let mut is_lock = false;
+        let mut filter_char = '#';
+        if x.lines().next().unwrap() == "#####" {
+            is_lock = true;
+            filter_char = '.';
+        }
         for line in x.lines() {
-            if line == "#####" {
-                is_lock = true;
-                continue;
+            line.chars()
+                .enumerate()
+                .filter(|(_idx, c)| c == &filter_char)
+                .for_each(|(idx, _c)| tmp_arr[idx] += 1);
+        }
+        if is_lock {
+            locks.push(tmp_arr);
+        } else {
+            keys.push(tmp_arr);
+        }
+    }
+
+    // info!(?locks);
+    // info!(?keys);
+
+    let mut count = 0;
+    for lock in locks {
+        for key in &keys {
+            //info!("lock: {lock:?} key: {key:?}");
+            let res = key.iter().zip(lock).all(|(a, b)| *a <= b);
+            //info!("==> res: {res}");
+            if res == true {
+                count += 1;
             }
         }
     }
 
-    0
+    count
 }
 
 #[cfg(test)]
