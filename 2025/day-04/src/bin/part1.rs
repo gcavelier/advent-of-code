5ba2a692b56ab0db7e75155990cfa6c92fc41f16
Gregory 'GaLi' Cavelier <gcavelier@cleia.org>
2025-12-04 08:18:02 +0100

    day 04 - part 1 & 2

---
+++ 2025/day-04/src/bin/part1.rs
@@ -0,0 +1,63 @@
+use itertools::Itertools;
+use pathfinding::grid::{self, Grid};
+use tracing::{debug, error, info, instrument};
+
+#[instrument(skip_all)]
+fn main() {
+    tracing_subscriber::fmt::init();
+
+    let input = include_str!("./input.txt");
+    let output = part1(input);
+    info!(output);
+}
+
+#[instrument(skip_all)]
+fn part1(input: &str) -> usize {
+    let mut res = 0;
+
+    let mut grid = Grid::new(
+        input.lines().nth(0).unwrap().chars().count(),
+        input.lines().count(),
+    );
+    grid.enable_diagonal_mode();
+
+    for (line_idx, line) in input.lines().enumerate() {
+        for (char_idx, char_value) in line.chars().enumerate() {
+            if char_value == '@' {
+                grid.add_vertex((char_idx, line_idx));
+            }
+        }
+    }
+
+    for roll in grid.iter() {
+        if grid.neighbours(roll).iter().count() < 4 {
+            res += 1;
+        }
+    }
+
+    res
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn part1_works() {
+        tracing_subscriber::fmt::init();
+        let result = part1(
+            "..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.",
+        );
+        assert_eq!(result, 13);
+    }
+}
